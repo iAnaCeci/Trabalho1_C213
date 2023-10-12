651ce91d7f6fc23ff9918f7e26de591cc97ee8dd
ana.cecilia@gec.inate.br <ana.cecilia@gec.inatel.br>
2023-10-12 10:36:59 -0300

    Initial commit

---
+++ smith.py
@@ -0,0 +1,30 @@
+import numpy as np
+import control as cnt
+import matplotlib.pyplot as plt
+
+
+
+#considerando uma função de transferencia em malha aberta FT=k/(tau*s+1)
+k=1.49
+tau=2.98
+Theta = 0.99 # atraso de propagação
+num = np. array ([k])
+den = np. array ([tau , 1])
+H = cnt.tf(num , den)
+n_pade = 20
+( num_pade , den_pade ) = cnt.pade ( Theta , n_pade )
+H_pade = cnt.tf( num_pade , den_pade )
+Hs = cnt.series (H , H_pade)
+Hmf = cnt.feedback(Hs, 1)
+
+t = np . linspace (0 ,40, 100)
+(t , y ) = cnt.step_response ( 3*Hs, t )
+(t , y1 ) = cnt.step_response ( 3*Hmf, t )
+plt.plot (t , y )
+plt.plot (t , y1 )
+plt.xlabel ( ' t [ s ] ')
+plt.ylabel('Amplitude')
+plt.title('')
+
+plt.grid ()
+plt.show()
